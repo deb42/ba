f811ed331d08143de1c939129cdd60b33b50ace8
David <david.bujok@gmx.de>
2015-03-26 00:01:48 +0100

    grundlagen druchgearbeitet

--- code/ajax.php
+++ code/ajax.php
@@ -1,14 +1,9 @@
 try {
-    if ($action === DSBUILDER_AJAX_ACTION_CHECK) {
-        $jsdot_graph_raw = required_param('jsdot_graph', PARAM_TEXT);
-        $result = $dsbuilder_ajax->action_check_valid_graph($jsdot_graph_raw);
-        $dsbuilder_ajax->add_to_log($action, $step_no);
-    } elseif ($action === DSBUILDER_AJAX_ACTION_NEXT_STEP) {
-        $jsdot_graph_raw = required_param('jsdot_graph', PARAM_TEXT);
-        $result = $dsbuilder_ajax->action_submit_current_step($jsdot_graph_raw);
-        $dsbuilder_ajax->add_to_log($action, $step_no);
-    } elseif ($action === DSBUILDER_AJAX_DELETE_LAST_STEP) {
-        $result = $dsbuilder_ajax->action_delete_last_step();
-        $dsbuilder_ajax->add_to_log($action, $step_no);
-    }
+  if ($action === DSBUILDER_AJAX_ACTION_CHECK) {
+    $result = $dsbuilder_ajax->action_check_valid_graph($jsdot_graph_raw, $additional_info);
+  } elseif ($action === DSBUILDER_AJAX_ACTION_NEXT_STEP) {
+    $result = $dsbuilder_ajax->action_submit_current_step($jsdot_graph_raw, $additional_info);
+  } elseif ($action === DSBUILDER_AJAX_DELETE_LAST_STEP) {
+    $result = $dsbuilder_ajax->action_delete_last_step();
+  }
 }
